f37bede5f237875af3653c71b4f3299909200f7c
Willian-Dos-Reis <106329846+Willian-Dos-Reis@users.noreply.github.com>
2022-07-20 22:20:57 -0300

    aaa

--- ex/sotest.js
+++ ex/sotest.js
@@ -2,7 +2,11 @@ function calcular(){
     var anodado = document.getElementById("anodado")
     var data = new Date()
     var anoatual = data.getFullYear() 
+    var res = document.getElementById("res")
 
     if (anodado.value.length == 0 || (anodado.value) > anoatual){
         window.alert("[ERRO ] Verifique se os dados estao corretos")}
-}
+         else {
+            var fsex = document.getElementsByName("sexo")
+            var idade = anoatual - (anodado.value)  
+        }
